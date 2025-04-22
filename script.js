bf11b83ca87cb1d992c9dc7c90cb0baa6e249a47
Nathiele Palavecini <145784922+nathielepalavecini@users.noreply.github.com>
2025-04-22 08:17:07 -0300

    22

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function(){
 
-     const botaoAcessibilidade = document.getElementById('botao-acessibilidade');
+     const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
      const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
-
-     botaoAcessibilidade.addEventListener('click',function(){
-          botaoAcessibilidade.classList.toggle('rotacao-botao');
-          opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+     
+     botaoDeAcessibilidade.addEventListener('click',function(){
+     botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+     opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
      })
      
-    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
-    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
-
-    let tamanhoAtualFonte = 1;
-
-    aumentaFonteBotao.addEventListener('click', function () {
-         tamanhoAtualFonte += 0.1;
-         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
-    });
-
-    diminuiFonteBotao.addEventListener('click', function () {
-        tamanhoAtualFonte -= 0.1;
-        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
-   });
-});
+     
+     
+     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+     
+     const alternaContraste = document.getElementById('alterna-contraste');
+     
+     let tamanhoAtualFonte = 1;
+     
+     aumentaFonteBotao.addEventListener('click', function(){
+     tamanhoAtualFonte += 0.1;
+     document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+     });
+     
+     diminuiFonteBotao.addEventListener('click', function(){
+     tamanhoAtualFonte -= 0.1;
+     document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+     });
+     
+     alternaContraste.addEventListener('click', function(){
+     document.body.classList.toggle('alto-contraste');
+     })
+     });
