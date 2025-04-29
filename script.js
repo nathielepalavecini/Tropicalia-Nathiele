3ae1134f5583860591d94810951dcedc7fb1f5e7
Nathiele Palavecini <145784922+nathielepalavecini@users.noreply.github.com>
2025-04-29 08:19:49 -0300

    1

--- script.js
+++ script.js
@@ -36,3 +36,6 @@ document.addEventListener('DOMContentLoaded', function(){
      });
 
      ScrollReveal().reveal('#inicio', {delay: 500});
+     ScrollReveal().reveal('#galeria', {delay: 500});
+     ScrollReveal().reveal('#tropicalia', {delay: 500});
+     ScrollReveal().reveal('#contato', {delay: 500});
